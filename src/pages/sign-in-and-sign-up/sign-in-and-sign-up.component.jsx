2bef470c60c256bebe9f82832744b45e5fb46706
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-10 03:21:47 -0400

    apply email and password authanticated method for sign up user usinng firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import "./sign-in-and-sign-up.style.sass";
+import "./sign-in-and-sign-up.style.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
